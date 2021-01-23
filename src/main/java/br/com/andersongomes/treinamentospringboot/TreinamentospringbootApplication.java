615bbbcf198dc40c974872315e9337d242bfe668
Anderson Castro <anderson.castro@saude.pe.gov.br>
2021-01-23 12:16:24 -0300

    primeiro commit

---
+++ src/main/java/br/com/andersongomes/treinamentospringboot/TreinamentospringbootApplication.java
@@ -0,0 +1,13 @@
+package br.com.andersongomes.treinamentospringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TreinamentospringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TreinamentospringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/andersongomes/treinamentospringboot/TreinamentospringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.andersongomes.treinamentospringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TreinamentospringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
